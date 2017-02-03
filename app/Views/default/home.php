06780bc94284dcdbf2c871d3cfe719a63b21a260
Kilian Tatain Mirail <tatain1@hotmail.com>
2017-02-03 14:23:34 +0100

    initialisation

---
+++ app/Views/default/home.php
@@ -0,0 +1,7 @@
+<?php $this->layout('layout', ['title' => 'Accueil']) ?>
+
+<?php $this->start('main_content') ?>
+	<h1>Kilian-Mirail.fr</h1>
+	<p>Site en construction.</p>
+	
+<?php $this->stop('main_content') ?>
